86887a5a2f4146559669a89670e117c4b5904414
Jesus Prieto <jesus.prieto@moixa.com>
2019-01-04 18:24:27 +0000

    Removing generated files

--- project_1/src/foo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var bar_1 = require("@projects/project_2/src/bar");
-var fn = function (a) { return a; };
-fn(bar_1.x);
-bar_1.bar();
-console.log(bar_1.x);
-//# sourceMappingURL=foo.js.map
